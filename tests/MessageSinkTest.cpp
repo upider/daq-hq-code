f467a42a2b85bbdca3818dbec377012ddfa1deb3
yzz <1445277435@qq.com>
2021-05-12 14:18:15 +0800

    GA

--- tests/MessageSinkTest.cpp
+++ tests/MessageSinkTest.cpp
@@ -1,33 +1,68 @@
 #include "message_sink/message_sink.h"
+#include "utils/cmdline.h"
+
 #include "TestDataMessage.h"
 
 using namespace message_pass;
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    std::shared_ptr<MessageSink<TestDataMessage>>
-            cs(new MessageSink<TestDataMessage>("192.168.37.204", 9999,
-                    "192.168.37.201:9092", {"test"}, 1));
+    cmdline::parser cmdline_parser;
+    cmdline_parser.add<std::string>("ip", 0, "server ip address");
+    cmdline_parser.add<int>("port", 0, "server port");
+    cmdline_parser.add<std::string>("topic_server", 0, "topic server address (kafka broker list)");
+    cmdline_parser.add<std::string>("topic", 0, "subscribe topic");
+    cmdline_parser.add<std::size_t>("io_threads", 0, "threads number for zmq");
+    cmdline_parser.add<std::string>("source_id", 0, "message source identity");
+    cmdline_parser.parse_check(argc, argv);
 
-    std::string topic{"test"};
-    std::string source{"centos204"};
-    cs->prepare_sources(topic, {source});
+    std::string topic = cmdline_parser.get<std::string>("topic");
+    std::string source = cmdline_parser.get<std::string>("source_id");
+
+    std::shared_ptr<MessageSink<TestDataMessage>>
+    cs(new MessageSink<TestDataMessage>(
+       cmdline_parser.get<std::string>("ip"),
+       cmdline_parser.get<int>("port"),
+       cmdline_parser.get<std::string>("topic_server"),
+       {topic},
+       cmdline_parser.get<std::size_t>("io_threads")
+       )
+    );
+
+    //提前准备好source
+    if(!source.empty()) {
+        cs->prepare_sources(topic, {source});
+    }
+    
     //start 必须在send request之前调用
     cs->start();
 
-    TestDataMessage* msg;
-    cs->send_get_request(topic);
+    std::atomic_bool run{true};
+    std::thread th([&run, cs, topic, source](){
+        while (run) {
+            TestDataMessage* msg;
 
-    while(!cs->get_msg(topic, source, 5000, &msg));
+            cs->send_get_request(topic);
 
-    char* str = new char[msg->size() + 1];
-    str[msg->size()] = '\0';
-    std::memcpy(str, msg->data(), msg->size());
-    std::cout << "recv " << msg->size() << " bytes message: " << str << "\n";
+            bool ret;
+            do {
+                ret = cs->get_msg(topic, source, 50, &msg);
+            } while(!ret && run);
+            
+            if(!ret) {
+                return;
+            }
+            
+            SPDLOG_INFO("recv {} bytes message, key is: {}", msg->size(), msg->key());
+            cs->send_del_request(topic, msg);
+            delete msg;
+        }
+    });
 
-    sleep(30);
+    sleep(60);
+    run = false;
+    th.join();
     cs->stop();
 
-    delete msg;
     return 0;
 }
--- tests/MessageSourceTest.cpp
+++ tests/MessageSourceTest.cpp
@@ -1,29 +1,59 @@
 #include <memory>
 #include <unistd.h>
+#include <thread>
 
 #include "message_source/message_source.h"
+#include "utils/cmdline.h"
+
 #include "TestDataMessage.h"
 
 using namespace message_pass;
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    std::string topic{"test"};
-    std::string id{"centos204"};
+    cmdline::parser cmdline_parser;
+    cmdline_parser.add<std::string>("topic_server", 0, "topic server address (kafka broker list)");
+    cmdline_parser.add<std::string>("topic", 0, "subscribe topic");
+    cmdline_parser.add<std::size_t>("io_threads", 0, "threads number for zmq");
+    cmdline_parser.add<std::string>("source_id", 0, "message source identity", false);
+    cmdline_parser.parse_check(argc, argv);
+
+    auto topic = cmdline_parser.get<std::string>("topic");
 
     std::shared_ptr<MessageSource<TestDataMessage>>
-            ps(new MessageSource<TestDataMessage>("192.168.37.201:9092", {topic}, 1));
+    ps(new MessageSource<TestDataMessage>
+    (
+       cmdline_parser.get<std::string>("topic_server"),
+       {topic},
+       cmdline_parser.get<std::size_t>("io_threads")
+    )
+    );
 
-    ps->set_identity(id);
+    auto id = cmdline_parser.get<std::string>("source_id");
+    if(!id.empty()) {
+        ps->set_identity(id);
+    }
+
     ps->start();
+    std::atomic_bool run{true};
+    std::thread th([&run, ps, topic](){
+        std::size_t i = 0;
+        while(run) {
+            TestDataMessage* msg = new TestDataMessage(500);
+            std::memcpy(msg->data(), &i, sizeof(i));
+            std::memcpy((char*)msg->data()+8, "hello world", 11);
+            std::memset((char*)msg->data()+19, 0, 481);
+            msg->size(500);
+            ps->send(topic, msg);
+            i++;
+            sleep(1);
+        }
+    });
 
-    TestDataMessage* msg = new TestDataMessage("hello world this is IHEP", 24, 24);
-    // msg->add("hello world", 11);
-    ps->send(topic, msg);
-
-    sleep(30);
+    sleep(60);
+    run = false;
+    th.join();
     ps->stop();
 
-    delete msg;
     return 0;
 }
